a7bb90b4030a0dccc93b308b65e5eaf4e330e2a2
pellytw <marcelo.eduardo.gonzalez@gmail.com>
2018-11-29 15:55:28 -0300

    cambios botones actions index

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -36,12 +36,12 @@
               <td>{{$car->model}}</td>
               <td>{{$car->description}}</td>
               <td>{{$car->price}}</td>
-              <td><a href="{{ route('cars.edit',$car->id)}}" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-             
-                  <form action="{{ route('cars.destroy', $car->id)}}" method="post" style="display:inline;">
+              <td><a href="{{ route('cars.edit',$car->id)}}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a></td>
+              <td>
+                  <form action="{{ route('cars.destroy', $car->id)}}" method="post">
                     @csrf
                     @method('DELETE')
-                    <button class="btn btn-danger btn-sm" type="submit"><i class="fas fa-trash-alt"></i></button>
+                    <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i></button>
                   </form>
               </td>
           </tr>
